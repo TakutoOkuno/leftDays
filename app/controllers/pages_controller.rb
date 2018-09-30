5f0bb4e63c44cf578d65aafc78b52e9af0337195
TakutoOkuno <takuto.okuno.ku0823@gmail.com>
2018-10-01 01:12:54 +0900

    Create Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def home
+    the_day = Time.local(2018, 12, 3, 14, 40, 00, 0)
+    the_day_int = the_day.to_i
+
+    now = Time.now
+    now_int = now.to_i
+
+    left_unix_secs = the_day_int - now_int
+    left_days = left_unix_secs / (60 * 60 * 24)
+    left_hours = left_unix_secs / (60 * 60) % 24
+    left_minutes = left_unix_secs / 60 % 60
+
+    ld = left_days
+    lh = left_hours
+    lm = left_minutes
+
+    @left_time_to_the_day = "フライトまであと#{ld}日#{lh}時間#{lm}分です"
+  end
+end
